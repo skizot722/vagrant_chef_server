e4b35a8a5b2832075bad7b48c40d4790a7d79889
skizot722 <scott.hunter.iii@gmail.com>
2012-07-25 20:34:34 -0500

    Replace hardcoded home path with #{ENV['HOME']}

--- knife.rb
+++ knife.rb
@@ -1,10 +1,10 @@
 log_level                :info
 log_location             STDOUT
 node_name                'knife-client-user'
-client_key               '/home/slhunter/.chef/knife-client-user.pem'
+client_key               "#{ENV['HOME']}/.chef/knife-client-user.pem"
 validation_client_name   'chef-validator'
 validation_key           '/etc/chef/validation.pem'
 chef_server_url          'http://localhost:44000'
 cache_type               'BasicFile'
-cache_options( :path => '/home/slhunter/.chef/checksums' )
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{ENV['HOME']}/chef-repository/cookbooks"]
--- knife_production.rb
+++ knife_production.rb
@@ -1,10 +1,10 @@
 log_level                :info
 log_location             STDOUT
 node_name                'slhunter'
-client_key               '/home/slhunter/.chef/slhunter.pem'
+client_key               "#{ENV['HOME']}/.chef/slhunter.pem"
 validation_client_name   'chef-validator'
 validation_key           '/etc/chef/validation.pem'
 chef_server_url          'http://chef.ccisystems.com:4000'
 cache_type               'BasicFile'
-cache_options( :path => '/home/slhunter/.chef/checksums' )
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{ENV['HOME']}/chef-repository/cookbooks"]
